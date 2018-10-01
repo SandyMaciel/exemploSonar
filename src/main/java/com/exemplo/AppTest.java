656b35f0beb279a8ee60e18b5d093753a076c6d4
sandytrilogo <sandy@trilogo.com.br>
2018-10-01 10:53:35 -0300

    Exemplo sonar

---
+++ src/main/java/com/exemplo/AppTest.java
@@ -0,0 +1,19 @@
+package com.exemplo;
+
+import junit.framework.*;
+
+public class AppTest extends TestCase {
+
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}
